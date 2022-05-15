316aa62e56a317bce804923674ad82adf92d1ad8
linkzzmbbsoffical <104633164+linkzzmbbsoffical@users.noreply.github.com>
2022-05-15 20:55:25 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -280,7 +280,7 @@ async def call_apropriate_function(
                         f"<a href='tg://user?id={user_id}'>Your Requested Files</a>\n\n"
                     )
                     message_to_send = mention_req_user + message_to_send
-                    message_to_send = message_to_send + "\n\n" + "#uploads"
+                    message_to_send = message_to_send + "\n\n" + "#uploads\n\n<b>💫 Powered By : @LinkZz_MBBS"
                 else:
                     message_to_send = "<i>FAILED</i> to upload files. 😞😞"
                 await user_message.reply_text(
